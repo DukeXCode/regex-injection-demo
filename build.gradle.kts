4d5e81814b0488db237b36d3b1dbf92ee086419d
DukeXCode <naim.giger@outlook.com>
2024-03-21 14:23:34 +0100

    remove unnecessary dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,6 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
